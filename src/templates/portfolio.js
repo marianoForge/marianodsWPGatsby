497c0e95f70a6c2956d40ef5baef66602dcb4c29
Mariano De Simone <mariano@bigbangla.biz>
2020-08-25 12:03:08 -0600

    First commit

---
+++ src/templates/portfolio.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Layout from "../components/layout"
+import styled from "styled-components"
+
+const FeaturedImage = styled.img`
+  max-width: 300px;
+  margin: 16px 0;
+`
+
+export default ({ pageContext }) => (
+  <Layout>
+    <div>
+      <h1>{pageContext.title}</h1>
+      <div>
+        <strong>Website Url: </strong>
+        <a
+          href={pageContext.acf.portfolio_url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {pageContext.acf.portfolio_url}
+        </a>
+      </div>
+      <FeaturedImage src={pageContext.featured_media.source_url} />
+      <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
+    </div>
+  </Layout>
+)
